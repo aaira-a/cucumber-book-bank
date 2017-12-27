a2014ce698db796ec1aeda2822eeac0ce2e2ec53
azam alias <azamalias@gmail.com>
2017-12-27 13:15:46 +0800

    added expectation to first step to verify the deposit step works

--- features/step_definitions/steps.rb
+++ features/step_definitions/steps.rb
@@ -1,6 +1,8 @@
 Given(/^I have deposited \$(\d+) in my account$/) do |amount|
   my_account = Account.new
   my_account.deposit(amount.to_i)
+  expect(my_account.balance).to eq(amount.to_i),
+    "Expected the balance to be #{amount} but it was #{my_account.balance}"
 end
 
 When(/^I request \$(\d+)$/) do |arg1|
@@ -13,5 +15,8 @@ end
 
 class Account
   def deposit(amount)
+  end
+
+  def balance
   end
 end
