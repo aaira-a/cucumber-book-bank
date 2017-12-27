c779d55a996fe5c6f1e2217a19f7ded30627cfa7
azam alias <azamalias@gmail.com>
2017-12-27 15:31:49 +0800

    reworded scenario, step and method (deposit - > credit) for consistency

--- features/step_definitions/account_steps.rb
+++ features/step_definitions/account_steps.rb
@@ -1,5 +1,5 @@
-Given(/^I have deposited \$(#{CAPTURE_CASH_AMOUNT}) in my account$/) do |amount|
-  my_account.deposit(amount)
+Given(/^my account has been credited with \$(#{CAPTURE_CASH_AMOUNT})$/) do |amount|
+  my_account.credit(amount)
   expect(my_account.balance).to eq(amount),
     "Expected the balance to be #{amount} but it was #{my_account.balance}"
 end
