7233ad1c0c6f2ec67bba38c3bc5e51fe072a0025
azam alias <azamalias@gmail.com>
2017-12-27 15:20:28 +0800

    extended scenario to catch balance not decreased bug

--- features/step_definitions/account_steps.rb
+++ features/step_definitions/account_steps.rb
@@ -3,3 +3,8 @@ Given(/^I have deposited \$(#{CAPTURE_CASH_AMOUNT}) in my account$/) do |amount|
   expect(my_account.balance).to eq(amount),
     "Expected the balance to be #{amount} but it was #{my_account.balance}"
 end
+
+Then(/^the balance of my account should be \$(\d+)$/) do |amount|
+  expect(my_account.balance).to eq(amount),
+    "Expected the balance to be #{amount} but it was #{my_account.balance}"
+end
