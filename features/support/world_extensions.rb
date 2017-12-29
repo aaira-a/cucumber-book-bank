7712ccd2d0ae37640adab7a323fb1331b617085c
azam alias <azamalias@gmail.com>
2017-12-29 16:47:52 +0800

    passed account parameter directly to sinatra for test session (more) as temporary workaround because there is no authentication yet.
    currently in sinatra, the test session will fail if the endpoint is called without a set account.
    following through the steps, the account used in test is actually the my_account instance

--- features/support/world_extensions.rb
+++ features/support/world_extensions.rb
@@ -4,6 +4,7 @@ module KnowsTheUserInterface
     include Capybara::DSL
 
     def withdraw_from(account, amount)
+      Sinatra::Application.account = account
       visit '/'
       fill_in 'Amount', :with => amount
       click_button 'Withdraw'
