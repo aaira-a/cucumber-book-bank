27027e95f9c29c50acd1d54a8fefddf15872a303
azam alias <azamalias@gmail.com>
2017-12-29 15:55:11 +0800

    enhanced user interface class to access application using capybara dsl

--- features/support/world_extensions.rb
+++ features/support/world_extensions.rb
@@ -1,7 +1,12 @@
 module KnowsTheUserInterface
   
   class UserInterface
+    include Capybara::DSL
+
     def withdraw_from(account, amount)
+      visit '/'
+      fill_in 'Amount', :with => amount
+      click_button 'Withdraw'
     end
   end
 
