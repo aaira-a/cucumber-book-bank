c5f1011d865d49a331ceb7412ef93d9b537c6a12
azam alias <azamalias@gmail.com>
2017-12-30 12:07:21 +0800

    used sampling instead of sleep by implementing eventually helper

--- features/step_definitions/account_steps.rb
+++ features/step_definitions/account_steps.rb
@@ -3,7 +3,5 @@ Given(/^my account has been credited with \$(#{CAPTURE_CASH_AMOUNT})$/) do |amou
 end
 
 Then(/^the balance of my account should be \$(\d+)$/) do |amount|
-  sleep 1
-  expect(my_account.balance).to eq(amount),
-    "Expected the balance to be #{amount} but it was #{my_account.balance}"
+  eventually { expect(my_account.balance).to eq(amount) }
 end
